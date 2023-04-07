ca43d5ab8bcd7f3d3cbb275d463585a93b25c4ac
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-07 20:31:42 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,1 +1,11 @@
 //code here
+import { createStore, combineReducers } from "redux";
+import tellZodiac from "./zodiac";
+
+const rootReducer = combineReducers({
+  tellZodiac,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
